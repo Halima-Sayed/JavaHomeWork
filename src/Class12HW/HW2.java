5821d3ad774000ad8d7672d989ee8cd910352c70
Halima-Sayed <halimasayed1999@gmail.com>
2023-04-24 13:28:24 -0400

    hw commit

---
+++ src/Class12HW/HW2.java
@@ -0,0 +1,15 @@
+package Class12HW;
+
+public class HW2 {
+    public static void main(String[] args) {
+        /*
+        Create a String and print it in reverse order (Sunday → yadnuS).
+         */
+
+        String color="Black";
+        for (int i = color.length()-1; i >=0; i--) {
+            System.out.print(color.charAt(i)+" ");
+        }
+    }
+    }
+
